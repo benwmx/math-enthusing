e07245ee07c4a0f2a0929e75033ea4a8e60443f4
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-09-30 20:20:07 +0100

    Fix error with calculate test.

--- src/__test__/calculate.test.js
+++ src/__test__/calculate.test.js
@@ -1,24 +1,24 @@
 import calculate from '../logic/calculate';
 
 let obj = [];
-let result = []; 
+let result = [];
 describe('Testing the Calculate Function', () => {
-  test('Test AC Button', () => {
-    obj = calculate(obj, 'AC');
-    result= {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    expect(obj).toBe(result);
-  });
+  // test('Test AC Button', () => {
+  //   obj = calculate(obj, 'AC');
+  //   result = {
+  //     total: null,
+  //     next: null,
+  //     operation: null,
+  //   };
+  //   expect(obj).toBe(result);
+  // });
   test('Test Buttons 1 0 + 5 = ', () => {
     obj = calculate(obj, '1');
     obj = calculate(obj, '0');
     obj = calculate(obj, '+');
     obj = calculate(obj, '5');
     obj = calculate(obj, '=');
-    result= {
+    result = {
       total: '15',
       next: null,
       operation: null,
@@ -29,7 +29,7 @@ describe('Testing the Calculate Function', () => {
     obj = calculate(obj, 'x');
     obj = calculate(obj, '2');
     obj = calculate(obj, '=');
-    result= {
+    result = {
       total: '30',
       next: null,
       operation: null,
@@ -40,7 +40,7 @@ describe('Testing the Calculate Function', () => {
     obj = calculate(obj, '÷');
     obj = calculate(obj, '3');
     obj = calculate(obj, '=');
-    result= {
+    result = {
       total: '10',
       next: null,
       operation: null,
@@ -49,9 +49,9 @@ describe('Testing the Calculate Function', () => {
   });
   test('Test Buttons % 4 =', () => {
     obj = calculate(obj, '%');
-    obj = calculate(obj, '4'); 
+    obj = calculate(obj, '4');
     obj = calculate(obj, '=');
-    result= {
+    result = {
       total: '2',
       next: null,
       operation: null,
@@ -60,9 +60,9 @@ describe('Testing the Calculate Function', () => {
   });
   test('Test Buttons - 6 =', () => {
     obj = calculate(obj, '-');
-    obj = calculate(obj, '6'); 
+    obj = calculate(obj, '6');
     obj = calculate(obj, '=');
-    result= {
+    result = {
       total: '-4',
       next: null,
       operation: null,
@@ -72,10 +72,10 @@ describe('Testing the Calculate Function', () => {
   test('Test Buttons +/- + 7 8 = ', () => {
     obj = calculate(obj, '+/-');
     obj = calculate(obj, '+');
-    obj = calculate(obj, '7'); 
+    obj = calculate(obj, '7');
     obj = calculate(obj, '8');
     obj = calculate(obj, '=');
-    result= {
+    result = {
       total: '82',
       next: null,
       operation: null,
@@ -87,11 +87,20 @@ describe('Testing the Calculate Function', () => {
     obj = calculate(obj, '.');
     obj = calculate(obj, '9');
     obj = calculate(obj, '=');
-    result= {
+    result = {
       total: '82.9',
       next: null,
       operation: null,
     };
     expect(obj.total).toBe(result.total);
+  });
+  test('Test AC Button', () => {
+    obj = calculate(obj, 'AC');
+    result = {
+      next: null,
+      total: null,
+      operation: null,
+    };
+    expect(obj).toStrictEqual(result);
   });
 });
